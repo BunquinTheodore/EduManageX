e8fe9e6aa9b68dd3aa09e6d2537e53769d5d5d3b
Theodore Bunquin <23-03102@g.batstate-u.edu.ph>
2024-11-29 01:37:30 +0800

    Update Department.java

--- src/main/java/model/model/Department.java
+++ src/main/java/model/model/Department.java
@@ -1,23 +1,28 @@
+// Define the package
 package model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Department {
+    // Declare the name of the department
     private String name;
+    // Declare the list to hold courses in the department
     private List<Course> courses;
 
+    // Constructor to initialize the department with a name
     public Department(String name) {
         this.name = name;
         this.courses = new ArrayList<>();
     }
 
+    // Getter method to get the name of the department
     public String getName() {
         return name;
     }
 
+    // Getter method to get the list of courses in the department
     public List<Course> getCourses() {
         return courses;
     }
 }
-
