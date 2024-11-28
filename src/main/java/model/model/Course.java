02ccee0fc7a03a825cbd3d4245b43db87217e28e
Theodore Bunquin <23-03102@g.batstate-u.edu.ph>
2024-11-29 01:36:30 +0800

    Update Course.java

--- src/main/java/model/model/Course.java
+++ src/main/java/model/model/Course.java
@@ -1,21 +1,27 @@
+// Define the package
 package model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Course {
+    // Declare the name of the course
     private String name;
+    // Declare the list to hold enrolled students
     private List<Student> enrolledStudents;
 
+    // Constructor to initialize the course with a name
     public Course(String name) {
         this.name = name;
         this.enrolledStudents = new ArrayList<>();
     }
 
+    // Getter method to get the name of the course
     public String getName() {
         return name;
     }
 
+    // Getter method to get the list of enrolled students
     public List<Student> getEnrolledStudents() {
         return enrolledStudents;
     }
